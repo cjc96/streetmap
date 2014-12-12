15977b166951a4a7c3c044541e2ab9dafd77d158
曹景辰 <caojingchen@caojingchendeMacBook-Pro.local>
2014-12-12 16:35:39 +0800

    Initial Commit

---
+++ Playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Playground
+//
+//  Created by 曹景辰 on 12/12/14.
+//  Copyright (c) 2014 曹景辰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
