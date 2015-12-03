12acecad3c60529e6078b5ec7414254b78779d6b
cjc <shcjc96@gmail.com>
2015-12-03 21:27:19 +0800

    hehe

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,17 +1,23 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <pugixml.hpp>
-#include <iostream>
-#include <assert.h>
-#include <map>
+//
+//  streetmap_all.h
+//  Playground
+//
+//  Created by 曹景辰 on 15/12/3.
+//  Copyright © 2015年 曹景辰. All rights reserved.
+//
+
+#include "streetmap_all.h"
 
 const char *source = "/Users/caojingchen/proj/cpp/Playground/Playground/map.xml";
 
 pugi::xml_document shmap;
 double minlat,minlon,maxlat,maxlon;
 
-typedef std::pair<uint32_t,uint32_t> point;
-std::map<uint32_t,point> points;
+typedef std::pair<uint64_t,uint64_t> point;
+std::map<uint64_t,point> points;
+
+char atom_window[] = "Drawing a simple map";
+cv::Mat atom_image = cv::Mat::zeros(w, w, CV_8UC3);
 
 void get_bounds(pugi::xml_node *now)
 {
@@ -24,21 +30,37 @@ void get_bounds(pugi::xml_node *now)
 void get_node(pugi::xml_node *now)
 {
     double templat,templon;
-    uint32_t temp1,temp2,id;
+    uint64_t temp1,temp2,id;
     point temp_point;
     
     id = now->attribute("id").as_uint();
-    templat = now->attribute("lax").as_double();
+    templat = now->attribute("lat").as_double();
     templon = now->attribute("lon").as_double();
-    temp1 = (templat - minlat) * 1000000;
-    temp2 = (templon - minlon) * 1000000;
+    temp1 = (templat - minlat) * 10000000;
+    temp2 = (templon - minlon) * 10000000;
     temp_point = std::make_pair(temp1, temp2);
     points[id] = temp_point;
+    
+    //printf("%.7lf %.7lf",templat,templon);
+    //getchar();
 }
 
 void get_way(pugi::xml_node *now)
 {
-    
+    for (pugi::xml_node temp = now->first_child(); temp != now->last_child(); temp = temp.next_sibling())
+    {
+        if (strcmp(temp.name(),"nd") == 0)
+        {
+            printf("%u\n",temp.attribute("ref").as_uint());
+        }
+    }
+}
+
+void debug()
+{
+    uint32_t x;
+    while (scanf("%d",&x), x != 0)
+        printf("%llu\n",points[x].first);
 }
 
 int main()
@@ -47,7 +69,7 @@ int main()
     
     if (!result)
     {
-        std::cout << "Something wrong with input" << std::endl;
+        printf("Something wrong with input!\n");
         assert(0);
     }
     
@@ -67,6 +89,7 @@ int main()
         }
     }
     
+    //debug();
     
     
     return 0;
---
+++ Playground/streetmap_all.h
@@ -0,0 +1,27 @@
+//
+//  streetmap_all.h
+//  Playground
+//
+//  Created by 曹景辰 on 15/12/3.
+//  Copyright © 2015年 曹景辰. All rights reserved.
+//
+
+#ifndef streetmap_all_h
+#define streetmap_all_h
+
+#include <stdint.h>
+#include <stdio.h>
+#include <pugixml.hpp>
+#include <assert.h>
+#include <map>
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/legacy/legacy.hpp>
+#include <opencv/cvaux.hpp>
+#include <vector>
+
+
+#endif /* streetmap_all_h */
